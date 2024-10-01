67f426cb6060a67cc6a0e797ab9544e8c4c4cbd9
anarodriguezj <112646656+anarodriguezj@users.noreply.github.com>
2024-10-01 11:42:31 +0200

    Add files via upload

---
+++ PoliceCar.cs
@@ -0,0 +1,78 @@
+﻿namespace Practice1
+{
+    class PoliceCar : Vehicle
+    {
+        //constant string as TypeOfVehicle wont change allong PoliceCar instances
+        private const string typeOfVehicle = "Police Car"; 
+        private bool isPatrolling;
+        private SpeedRadar speedRadar;
+        private PoliceStation policeStation;
+
+        public PoliceCar(string plate, PoliceStation policeStation) : base(typeOfVehicle, plate)
+        {
+            isPatrolling = false;
+            speedRadar = new SpeedRadar();
+            this.policeStation = policeStation;
+        }
+
+        public void UseRadar(Vehicle vehicle)
+        {
+            if (isPatrolling)
+            {
+                speedRadar.TriggerRadar(vehicle);
+                string meassurement = speedRadar.GetLastReading();
+                Console.WriteLine(WriteMessage($"Triggered radar. Result: {meassurement}"));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage($"has no active radar."));
+            }
+        }
+
+        public bool IsPatrolling()
+        {
+            return isPatrolling;
+        }
+
+        public void StartPatrolling()
+        {
+            if (!isPatrolling)
+            {
+                isPatrolling = true;
+                Console.WriteLine(WriteMessage("started patrolling."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage("is already patrolling."));
+            }
+        }
+
+        public void EndPatrolling()
+        {
+            if (isPatrolling)
+            {
+                isPatrolling = false;
+                Console.WriteLine(WriteMessage("stopped patrolling."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage("was not patrolling."));
+            }
+        }
+
+        public void PrintRadarHistory()
+        {
+            Console.WriteLine(WriteMessage("Report radar speed history:"));
+            foreach (float speed in speedRadar.SpeedHistory)
+            {
+                Console.WriteLine(speed);
+            }
+        }
+
+        public void ActivateAlarm()
+        {
+            if 
+        }
+
+    }
+}
---
+++ PoliceStation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class PoliceStation
+    {
+        private bool activeAlarm;
+        public List<PoliceCar> PoliceCarsList { get; private set; }
+
+        public PoliceStation()
+        {
+            activeAlarm = false;
+            PoliceCarsList = new List<PoliceCar>();
+        }
+
+        public void NotifyPlate()
+        {
+            if (activeAlarm)
+            {
+                foreach (PoliceCar PoliceCar in PoliceCarsList)
+                {
+
+                }
+            }
+
+            else
+            {
+
+            }
+        }
+    }
+}
